6e53a29bdc840dfe133a50000e00981bf3925c42
Tyuiu.BuynovaVI@outlook.com <v.buyanova1@mail.ru>
2024-10-15 19:12:35 +0500

    Task3

--- Tyuiu.BuyanovaVI.Sprint1.Task3.V17/Program.cs
+++ Tyuiu.BuyanovaVI.Sprint1.Task3.V17/Program.cs
@@ -1,10 +1,44 @@
-﻿namespace Tyuiu.BuyanovaVI.Sprint1.Task3.V17
+﻿using Tyuiu.BuyanovaVI.Sprint1.Task3.V17.Lib;
+
+namespace Tyuiu.BuyanovaVI.Sprint1.Task3.V17
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            DataService ds = new DataService();
+
+
+            Console.Title = "Спринт #1 | Выполнилa: Буянова В. И. | СМАРТб-24-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #1                                                               *");
+            Console.WriteLine("* Тема: Операторы составного присваивания                                 *");
+            Console.WriteLine("* Задание #3                                                              *");
+            Console.WriteLine("* Вариант #17                                                             *");
+            Console.WriteLine("* Выполнилa: Буянова В. И. | СМАРТб-24-1                                  *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать программу, которая определяет, есть ли среди первых трех цифр  *");
+            Console.WriteLine("* из дробной части заданного вещественного числа цифра 0                  *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+
+            double number = 1.3202;
+            var result = ds.ZeroCheck(number);
+            Console.WriteLine($"Проверим число {number} на наличие нуля после трех цифр из дробной части.");
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine();
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ                                                               *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine($"Число {number} содержит ноль в первых трёх знаках после запятой: {result}");
+
+            Console.ReadKey();
         }
     }
 }
